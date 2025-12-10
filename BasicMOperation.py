751aed16c4ed737f8cf08752af9e73e9efedc246
rohit142520 <rohit142520@gmail.com>
2025-12-10 16:35:20 +0530

    Add files via upload
    basic python assignment completed

---
+++ BasicMOperation.py
@@ -0,0 +1,12 @@
+#task 1: Perform basic mathematical operations
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+x=a+b
+y=a-b
+z=a*b
+w=a/b
+print("Addition of a and b: ",x)
+print("Subtraction of a and b: ", y)
+print("Multiplication of a and b: ", z)
+print("Division of a by b: ", round(w,2))
+print("This code written by Rohit Gautam whose mobile number is 9555328875.")
---
+++ Personlizegreetingmsg.py
@@ -0,0 +1,5 @@
+#Create a personalize greeting message
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print("Hello,", first_name,last_name,",Hurrah! You have been selected in our MNC with 120% hike salary.")
+print("This code written by Rohit Gautam whose mobile number is 9555328875.")
